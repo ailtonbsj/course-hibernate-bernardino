489e7fe06211a024a3eab349b0574b6e4017f193
ailtonbsj <ailton.ifce@gmail.com>
2019-12-10 16:33:50 -0300

    Using ManyToMany Birectional

--- rev-2.0-hibernate/src/main/java/curso/hibernate/teste/FuncionarioTeste.java
+++ rev-2.0-hibernate/src/main/java/curso/hibernate/teste/FuncionarioTeste.java
@@ -14,22 +14,34 @@ public class FuncionarioTeste {
 
 		session.beginTransaction();
 		
-		Funcionario funcionario = new Funcionario();
-		funcionario.setNome("João Batista");
-		funcionario.setSalario(2000.0);
+		Funcionario funcionario1 = new Funcionario();
+		funcionario1.setNome("João Batista");
+		funcionario1.setSalario(2000.0);
+
+		Funcionario funcionario2 = new Funcionario();
+		funcionario2.setNome("Manoel Oliveira");
+		funcionario2.setSalario(2200.0);
 		
 		Projeto projeto1 = new Projeto();
 		projeto1.setNome("Projeto 1");
+		projeto1.getFuncionarios().add(funcionario1);
+		projeto1.getFuncionarios().add(funcionario2);
 		
 		Projeto projeto2 = new Projeto();
 		projeto2.setNome("Projeto 2");
+		projeto2.getFuncionarios().add(funcionario1);
+		projeto2.getFuncionarios().add(funcionario2);
 		
-		funcionario.getProjetos().add(projeto1);
-		funcionario.getProjetos().add(projeto2);
+		funcionario1.getProjetos().add(projeto1);
+		funcionario1.getProjetos().add(projeto2);
+		
+		funcionario2.getProjetos().add(projeto1);
+		funcionario2.getProjetos().add(projeto2);
 		
 		session.save(projeto1);
 		session.save(projeto2);
-		session.save(funcionario);
+		session.save(funcionario1);
+		session.save(funcionario2);
 		session.getTransaction().commit();
 		session.close();
 	}
